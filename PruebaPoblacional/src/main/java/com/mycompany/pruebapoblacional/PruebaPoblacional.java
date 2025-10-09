166b77f376415e73fb9bde92f81d9529fcb8ba4f
Yung Samy <113071996+yungsamy4p@users.noreply.github.com>
2025-10-09 09:48:29 -0300

    Creación de proyecto
    NetBeans create

---
+++ PruebaPoblacional/src/main/java/com/mycompany/pruebapoblacional/PruebaPoblacional.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebapoblacional;
+
+/**
+ *
+ * @author Santo Tomas
+ */
+public class PruebaPoblacional {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
